1a97c783d06253d0ff20395552685457855c8db1
LiudmylaStrashko <ludmila.str0405@gmail.com>
2024-06-17 13:25:07 -0400

    all message

---
+++ src/day04_variables/EsacpeSequence.java
@@ -0,0 +1,12 @@
+package day04_variables;
+/*
+    Create a class called EscapeSequence2
+            Create a main method
+            Output this exactly:
+
+                "iphone", "laptop", "xbox", "webcam", "monitor"
+     */
+public class EsacpeSequence {public static void main (String [] args) {
+    System.out.println(" \"iphone\", \"laptop\", \"xbox\", \"webcam\", \"monitor\"   ");
+}
+}
