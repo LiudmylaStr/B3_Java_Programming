0d1dc5368a486c36a1d3e21b852e00a01fe72418
LiudmylaStrashko <ludmila.str0405@gmail.com>
2024-06-17 13:10:23 -0400

    all message

---
+++ src/day02_print_statements/TellMeAboutYouself.java
@@ -0,0 +1,9 @@
+package day02_print_statements;
+
+public class TellMeAboutYouself {
+    public static void main(String[] args) {
+        System.out.println("MY name is Myla. ");
+        System.out.println("I am from Ukraine");
+
+    }
+}
---
+++ src/day03_comments_escape_sequence/Goals.java
@@ -0,0 +1,9 @@
+package day03_comments_escape_sequence;
+
+public class Goals {
+    public static void main(String[] args) {
+        System.out.println("Love Java");
+        System.out.println("Have a great time");
+        System.out.println("Be better than I was yesterday");
+    }
+}
